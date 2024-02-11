699a7439913d3f397ac810916dab3016c89b114c
Coletti <matheus.coletti08@gmail.com>
2024-02-10 23:20:25 -0300

    update resources

--- choice/script.js
+++ choice/script.js
@@ -1,7 +1,7 @@
 document.getElementById("button1").addEventListener("click", function() {
-    window.location.href = "/photos/carousel.html";
+    window.location.href = "/choice/photos/carousel.html";
 });
 
 document.getElementById("button2").addEventListener("click", function() {
-    window.location.href = "/quiz/quiz.html";
+    window.location.href = "/choice/quiz/quiz.html";
 });
