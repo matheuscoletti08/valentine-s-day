8b2afd6ab8ae8195f9337238883265532bc59dad
Coletti <matheus.coletti08@gmail.com>
2024-02-12 20:45:06 -0300

    update resources

--- choice/script.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("button1").addEventListener("click", function() {
-    window.location.href = "carousel.html";
-});
-
-document.getElementById("button2").addEventListener("click", function() {
-    window.location.href = "quiz.html";
-});
